5e7877b348b8def6d6e54167fea7572d8d69ab89
Samarth M M <samarthmm7@gmail.com>
2024-12-27 22:59:42 +0530

    Added Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
 export default function Home() {
     return (
-        <div className="">
-            
+        <div className="w-full flex flex-col h-[80%] justify-center gap-10">
+            <div className="w-full flex flex-col items-center justify-center">
+                <h1 className="text-8xl font-semibold my-2">Uniblox</h1>
+                <h2 className="text-xl font-light text-gray-400">1M+ Products, 10M+ Users, 100M+ Transactions</h2>
+            </div>
+            <div className="">
+                <div className="w-full flex justify-center gap-4">
+                    <button className="bg-gradient-to-r from-violet-400 to-blue-400 text-white font-bold py-2 px-4 rounded-full">
+                        Products
+                    </button>
+                    <button className="bg-white text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded-full">
+                        Learn More
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
